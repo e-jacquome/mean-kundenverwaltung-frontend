3441e270158d8dad89c0c814c1312c91df756349
Eric Jacquome <ericj.kit@yahoo.com>
2020-01-13 16:03:24 +0100

    created address class and fixed the date problem

--- src/app/kunde/shared/kunde.service.ts
+++ src/app/kunde/shared/kunde.service.ts
@@ -211,8 +211,7 @@ export class KundeService {
         errorFn: (status: number, errors: { [s: string]: any }) => void,
     ) {
         console.log('KundeService.save(): kunde=', kunde);
-        kunde.geburtsdatum = new Date();
-        //kunde.adresse = new Adresse("67069", "Ludwigshafen");
+        //kunde.geburtsdatum = new Date();
 
         const errorFnPost = (err: HttpErrorResponse) => {
             if (err.error instanceof Error) {
